c23300ecda93cf0cb441fb07aabf0fe6067e895d
David Dorado <dam1adaviddoradocarvajal@iesantoniogala.es>
2021-05-21 19:09:08 +0200

    modificados Login y index

--- resources/views/lobby.blade.php
+++ resources/views/lobby.blade.php
@@ -5,17 +5,34 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Aplicacion pendientes</title>
+    <!-- Lineas para añadir bootstrap, se cuenta desde index.php -->
+    <link rel="stylesheet" href="css/bootstrap.min.css">
 </head>
-<body>
-
-
-
-    <h1>Bienvenido a la aplicacion pendientes</h1>
-    <h2>Que desea hacer?</h2>
-    <a href="/login">Iniciar Sesion</a>
-    <a href="/registrar">Registrar</a>
+<body class="text-center">
+    <div class="container">
+        <div class="row">
+            <div class="col-12">
+                <h1>Bienvenido a la aplicacion de seguimiento de materias pendientes</h1>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-12">
+                <h2>¿Que deseas hacer?</h2>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-7">
+                <a class="btn btn-primary" href="/login" role="button">Iniciar Sesion</a>
+            </div>
+            <div class="col-2">
+                <a class="btn btn-primary" href="/registrar" role="button">Registrar una cuenta</a>
+            </div>
+        </div>
     @if (isset($tieneSesion) && $tieneSesion === true)
         <a href="/home">Home</a>
     @endif
+
+    <!-- lineas para script se añaden al final y tambien se cuentan desde index.php -->
+    <script src="js/bootstrap.bundle.min.js"></script>
 </body>
 </html>
--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -5,25 +5,34 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Document</title>
+    <!-- Lineas para añadir bootstrap, se cuenta desde index.php -->
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <link href="css/login.css" rel="stylesheet">
 </head>
-<body>
-    <div>
-        <div>
-            <h1>Bienvenido al login</h1>
-            <h2>Por favor, introduzca sus datos</h2>
-
+<body class="text-center">
+    <main class="form-signin">
+        <form action="{{ route('auth.loguear') }}" method="post">
+            <h1 class="h3 mb-3 fw-normal">Vamos a iniciar sesión</h1>
+            <h2 class="h3 mb-3 fw-normal">Por favor, introduzca sus datos</h2>
             @if (isset($valor) && $valor == true)
                 <h3>Contraseña o usuario incorrectos</h3>
             @endif
+          <div class="form-floating">
+            @csrf
+            <input type="email" class="form-control" id="floatingInput" placeholder="nombre@iesantoniogala.es">
+            <label for="floatingInput">Correo Corporativo</label>
+          </div>
+          <div class="form-floating">
+            <input type="password" class="form-control" id="floatingPassword" placeholder="Password">
+            <label for="floatingPassword">Contraseña</label>
+          </div>
 
-            <form action="{{ route('auth.loguear') }}" method="post">
-                @csrf
-                <p>Email: <input type="email" name="email" id=""></p>
-                <p>Contraseña: <input type="password" name="password" id=""></p>
-                <input type="submit" value="Entrar">
-            </form>
-            <a href="/">Volver</a>
-        </div>
-    </div>
+          <button class="w-100 btn btn-lg btn-primary" type="submit">Sign in</button>
+        </form>
+        <a class="btn btn-primary" href="/" role="button">Volver</a>
+      </main>
+
+    <!-- lineas para script se añaden al final y tambien se cuentan desde index.php -->
+    <script src="js/bootstrap.bundle.min.js"></script>
 </body>
 </html>
